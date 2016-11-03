d75f069e1f89a06d702fad08bbbb17459b426622
paulzhang <paul8263@gmail.com>
2016-11-03 14:21:43 +1100

    finished error pages

--- src/main/java/com/paultech/web/ExceptionControllerAdvice.java
+++ src/main/java/com/paultech/web/ExceptionControllerAdvice.java
@@ -2,8 +2,10 @@ package com.paultech.web;
 
 import com.paultech.web.exceptions.ItemNotFoundException;
 import com.paultech.web.exceptions.UnauthorizedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Created by paulzhang on 2/11/2016.
@@ -18,5 +20,14 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler(value = UnauthorizedException.class)
     public String displayUnauthorizedPage() {
         return "errorPages/unauthorized";
+    }
+
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
+    public String displayForbiddenPage() {
+        return "errorPages/unauthorized";
+    }
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public String display404Page() {
+        return "errorPages/itemNotFound";
     }
 }
